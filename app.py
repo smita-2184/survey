f18cbbc2d239f05879e0de09afed0d088e7c530b
smita-2184 <151066791+smita-2184@users.noreply.github.com>
2024-06-04 22:40:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def get_random_link(links, link_counts):
 # Function to update link usage counts
 def update_link_counts(link, link_counts):
     link_counts[link] += 1
-with st.sidebar;
+with st.sidebar:
     
 with st.popover("Contact us📧"):
     st.markdown("Email us @ studium-eduai@gmail.com")
